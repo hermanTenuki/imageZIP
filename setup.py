fdc52e16bfc57697b5936107a369be4a61eb8f88
hermanTenuki <boss.warning@gmail.com>
2020-12-02 19:13:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
-import imageZIP
 
 with open("README.md", "r") as file:
     long_description = file.read()
 
 setup(name='imageZIP',
-      version=imageZIP.__version__,
+      version='3.1',
       description='Archive (encrypt) files and directories to image file',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='http://github.com/hermanTenuki/imageZIP/',
-      author=imageZIP.__author__,
-      author_email=imageZIP.__email__,
-      license=imageZIP.__license__,
+      author='Herman Schechkin (hermanTenuki)',
+      author_email='itseasy322@gmail.com',
+      license='MIT',
       packages=['imageZIP'],
       classifiers=[
             'Development Status :: 5 - Production/Stable',
